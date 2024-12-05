17b0efbe2173964f73d6fa5b615d071e7fc2becd
MariaJesus12 <marychuartaviasol2606@gmail.com>
2024-12-04 18:07:55 -0600

    1
    11

--- API_Web2/Configuration/conexion.php
+++ API_Web2/Configuration/conexion.php
@@ -3,7 +3,16 @@ class Conectar {
     protected $conexion_bd;
     protected function conectar_bd() {
         try {
-            $conexion = $this->conexion_bd = new PDO("mysql:host=localhost;dbname=biblioteca", "root", "");
+            $DB_HOST=$_ENV["DB_HOST"];
+            $DB_USER=$_ENV["DB_USER"];
+            $DB_PASSWORD=$_ENV["DB_PASSWORD"];
+            $DB_NAME=$_ENV["DB_NAME"];
+            $DB_PORT=$_ENV["DB_PORT"];
+            
+           
+          
+
+            $conexion = $this->conexion_bd = new PDO("$DB_HOST","$DB_USER","$DB_PASSWORD","$DB_NAME","$DB_PORT");
             return $conexion;
         } catch (Exception $e) {
             print "Error en la base de datos: " . $e->getMessage() . "<br/>";
