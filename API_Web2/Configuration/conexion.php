5895a2060a0bb8050c6239dc408baa9dbecddc52
MariaJesus12 <marychuartaviasol2606@gmail.com>
2024-12-04 22:48:33 -0600

    4
    444

--- API_Web2/Configuration/conexion.php
+++ API_Web2/Configuration/conexion.php
@@ -3,8 +3,12 @@ class Conectar {
     protected $conexion_bd;
     protected function conectar_bd() {
         try {
-          
-            $conexion = $this->conexion_bd = new PDO("mysql://root:bxTcLeqRPJZsNgGokCXtQtILppbmmARL@autorack.proxy.rlwy.net:13759/railway");
+          $conexion=$this->conexion_bd=new PDO(
+            "mysql:host=mysql.railway.internal;port=3306;dbname=railway",
+            "root",
+            "bxTcLeqRPJZsNgGokCXtQtILppbmmARL"
+          );
+        $this->conexion_bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conexion;
         } catch (Exception $e) {
             print "Error en la base de datos: " . $e->getMessage() . "<br/>";
